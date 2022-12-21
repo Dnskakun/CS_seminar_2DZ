5f36d9ae3bed86d39ce5a1759e329ac9c80dd703
dnskakun <dimon_sdn@list.ru>
2022-12-21 22:03:14 +0300

    Добавил задачу №1

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Программа принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+
+int UserInput()
+{
+    int number;
+    while (true)
+    {
+        Console.Write("Введите трехзначное целое число: ");
+        if (int.TryParse(Console.ReadLine(), out number))
+            break;
+        Console.WriteLine("Ошибка ввода!");
+    }
+    return number;
+}
+
+int userInput1 = UserInput();
+
+int des = (userInput1 - userInput1 / 100 * 100) / 10;
+
+Console.WriteLine($"{userInput1} -> {des}");
