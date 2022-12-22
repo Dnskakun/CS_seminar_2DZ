85bcbe092baddd8f8d9e3652e4f2d26e28e26845
dnskakun <dimon_sdn@list.ru>
2022-12-22 11:42:15 +0300

    Добавил задачу №2

---
+++ Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿//Программа выводит третью цифру слева у заданного числа или сообщает, что третьей цифры нет
+
+int UserInput()
+{
+    int number;
+    while (true)
+    {
+        Console.Write("Введите целое положительное число: ");
+        if (int.TryParse(Console.ReadLine(), out number))
+            break;
+        Console.WriteLine("Ошибка ввода!");
+    }
+    return number;
+}
+
+int userInput1 = UserInput();
+int div = 100;
+int tmp = 1;
+
+if (userInput1 > 99)
+{
+    while (tmp > 0)
+        {
+            tmp = userInput1 / div;
+            if (tmp == 0) break;
+            div = div * 10;
+        }
+
+    int result = userInput1 % (div / 100) / (div / 1000);
+
+    Console.WriteLine($"{userInput1} -> {result}");
+}
+else Console.WriteLine($"{userInput1} -> третьей цифры нет.");
+
