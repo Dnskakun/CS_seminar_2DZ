518ba4716600556a3fb6c68075fcd711a3f0b2d3
dnskakun <dimon_sdn@list.ru>
2022-12-22 11:56:28 +0300

    Добавил задачу №3

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Программа принимает на вход цифру (от 1 до 7 включительно) являющуюся днем недели и проверят является ли этот день выходным
+
+int UserInput()
+{
+    int number;
+    while (true)
+    {
+        Console.Write("Введите цифру, обозначающую день недели (диапазон от одного до 7): ");
+        if (int.TryParse(Console.ReadLine(), out number))
+            break;
+        Console.WriteLine("Ошибка ввода!");
+    }
+    return number;
+}
+
+int userInput1 = UserInput();
+
+if (userInput1 == 6) Console.WriteLine("Введеная цифра соответствует субботе.");
+else if (userInput1 == 7) Console.WriteLine("Введеная цифра соответствует воскресенью.");
+else Console.WriteLine("Введенная цифра не соответствует выходному дню.");
